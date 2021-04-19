6a22238706b98e0251f09ecb6e82390d8e6be79c
Dominic <dominic.pr93@gmail.com>
2021-04-19 18:32:41 +0200

    added navigation to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/globals.scss"
 
+import Navigation from "../components/navigation"
 import type { AppProps /*, AppContext */ } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<div className="">
+			<Navigation />
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
