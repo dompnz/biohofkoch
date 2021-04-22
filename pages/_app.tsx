9037391845c4f4aeed4e4a2cbf6e3af754423150
Dominic <dominic.pr93@gmail.com>
2021-04-22 23:24:13 +0200

    cms updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.scss"
+import Head from "next/head"
 
 import type { AppProps /*, AppContext */ } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div className="font-sans text-gray-dark">
+			<Head>
+				<title></title>
+			</Head>
 			<Component {...pageProps} />
 		</div>
 	)
