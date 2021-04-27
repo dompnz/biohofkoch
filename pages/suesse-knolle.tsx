33feb96b6550f87d3db472e7d480ab7de8d0392c
Dominic <dominic.pr93@gmail.com>
2021-04-27 13:02:38 +0200

    added example usage of react-markdown for multiple markdown attributes inisde one markdown file

--- pages/suesse-knolle.tsx
+++ pages/suesse-knolle.tsx
@@ -1,9 +1,12 @@
 import Layout from "../components/layout"
+import ReactMarkdown from "react-markdown"
 
 export default function Page() {
 	return (
 		<Layout>
-			<div></div>
+			<div>
+				<ReactMarkdown children={""} />
+			</div>
 		</Layout>
 	)
 }
