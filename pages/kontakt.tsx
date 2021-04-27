bbf33b4e49c1d4b623730c7f76d6d6b482cbea48
Dominic <dominic.pr93@gmail.com>
2021-04-28 00:25:50 +0200

    from test

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,9 +1,25 @@
 import Layout from "../components/layout"
 
-export default function Page() {
+export default function Contact() {
 	return (
 		<Layout>
-			<div></div>
+			<div className="mt-40">
+				<form name="contact" data-netlify="true">
+					<p className="mb-4">
+						<label>
+							Name <input type="text" name="name" />
+						</label>
+					</p>
+					<p className="mb-4">
+						<label>
+							Email <input type="email" name="email" />
+						</label>
+					</p>
+					<p>
+						<button type="submit">Send</button>
+					</p>
+				</form>
+			</div>
 		</Layout>
 	)
 }
