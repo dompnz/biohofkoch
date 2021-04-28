d54bbe1804eaf8a7496d3c6e4c16dea57177fce4
Dominic <dominic.pr93@gmail.com>
2021-04-28 09:17:14 +0200

    form test

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,24 +1,40 @@
 import Layout from "../components/layout"
+import { useState, useEffect } from "react"
 
 export default function Contact() {
+	const [success, setSuccess] = useState(false)
+
+	useEffect(() => {
+		if (window.location.search.includes("success=true")) {
+			setSuccess(true)
+		}
+	}, [])
+
 	return (
 		<Layout>
 			<div className="mt-40">
-				<form name="contact" data-netlify="true">
-					<p className="mb-4">
+				<form name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+					<input type="hidden" name="form-name" value="contact" />
+					<p>
 						<label>
-							Name <input type="text" name="name" />
+							Your Name: <input type="text" name="name" />
 						</label>
 					</p>
-					<p className="mb-4">
+					<p>
 						<label>
-							Email <input type="email" name="email" />
+							Your Email: <input type="email" name="email" />
+						</label>
+					</p>
+					<p>
+						<label>
+							Message: <textarea name="message"></textarea>
 						</label>
 					</p>
 					<p>
 						<button type="submit">Send</button>
 					</p>
 				</form>
+				{success && <p style={{ color: "green" }}>Successfully submitted form!</p>}
 			</div>
 		</Layout>
 	)
