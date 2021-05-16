e64d7f82a7db64f0e5dfa0c98c37ec64507e5e65
Dominic <dominic.pr93@gmail.com>
2021-05-16 21:19:49 +0200

    typography style fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,23 +24,21 @@ export default function Placeholder() {
 						className="lg:hidden absolute left-1/2 md:left-8 top-full transform -translate-x-1/2 md:translate-x-0 -translate-y-1/2 w-1/3 sm:w-1/4 md:w-1/5"
 					/>
 					{/* desktop logo */}
-					<img
-						src="/assets/images/logo-with-border.svg"
-						alt="Biohof Koch Logo"
-						className="hidden lg:block absolute left-full top-1/3 transform -translate-x-1/2 -translate-y-1/2 w-1/2 w-[345px]"
-					/>
+					<img src="/assets/images/logo-with-border.svg" alt="Biohof Koch Logo" className="hidden lg:block absolute left-full top-1/3 transform -translate-x-1/2 -translate-y-1/2 w-1/2 w-[345px]" />
 				</div>
 				{/* placeholder for the overlapping part of the visible mobile logo. should always have 1/2 of its width. */}
 				<img src="/assets/images/logo-with-border.svg" alt="Logo Placeholder" className="invisible w-1/6 sm:w-1/8 md:w-1/10 lg:hidden" />
 				{/* info container */}
 				<div className="container max-w-none flex-grow mt-8 pb-12 lg:mt-0 lg:p-12 xl:p-20 flex flex-col lg:w-1/3">
 					<div className="mt-auto mb-6 lg:mb-16 animated-underlines">
-						<h1 className="subheader text-sm mb-4">Biohof Koch</h1>
-						<MarkdownContent />
+						<h1 className="subheader mb-4">Biohof Koch</h1>
+						<div className="p-no-mb">
+							<MarkdownContent />
+						</div>
 					</div>
 					<a href={attributes.facebooklink} target="_blank" rel="noreferrer" className="flex items-center flex-wrap animated-underlines">
-						<img src="/assets/images/facebook.svg" alt="facebook icon" width="22px" height="22px" className="inline mr-5 object-contain object-center" />
-						<span className="subheader text-sm animated-underlines-target">Biohof Koch</span>
+						<img src="/assets/images/facebook.svg" alt="facebook icon" width="22px" height="22px" className="inline mr-4 object-contain object-center" />
+						<span className="subheader animated-underlines-target">Biohof Koch</span>
 					</a>
 				</div>
 			</div>
