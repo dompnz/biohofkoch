048025e1288cd5dd6388bdec8973f6103f3a63a3
Dominic <dominic.pr93@gmail.com>
2021-06-27 19:16:52 +0200

    added theme-color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import "../styles/globals.scss"
-import Head from "next/head"
+import '../styles/globals.scss'
+import Head from 'next/head'
 
-import type { AppProps /*, AppContext */ } from "next/app"
+import type { AppProps /*, AppContext */ } from 'next/app'
+import tailwindConfig from '../tailwind.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className="font-sans text-gray-dark bg-gray-bg text-base break-words min-h-screen">
+		<div className='font-sans text-gray-dark bg-gray-bg text-base break-words min-h-screen'>
 			<Head>
-				<meta property="og:type" content="website" />
-				<meta property="og:locale" content="de_AT" />
+				<meta property='og:type' content='website' />
+				<meta property='og:locale' content='de_AT' />
+				{/* background color for mobiles */}
+				<meta name='theme-color' content={tailwindConfig.theme.extend.colors.gray.bg} />
 			</Head>
 			<Component {...pageProps} />
 		</div>
