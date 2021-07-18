36d8f04d8b4f94f49851ec519ed02aa6be5f3981
Dominic <dominic.pr93@gmail.com>
2021-07-19 01:38:59 +0200

    added basic animations on page change

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,11 +1,27 @@
 import Navigation from '../navigation'
 import Footer from '../footer'
+import { motion } from 'framer-motion'
 
 export default function Layout({ children, showNav = true, useNavSpacer = true, showFooter = true }) {
+	const variants = {
+		hidden: { opacity: 0, x: -500, y: 0 },
+		enter: { opacity: 1, x: 0, y: 0 },
+		exit: { opacity: 0, x: 500, y: 0 },
+	}
+
 	return (
 		<div className='flex flex-col min-h-screen'>
 			{showNav && <Navigation useNavSpacer={useNavSpacer} />}
-			<main className='mb-auto'>{children}</main>
+			<motion.main
+				variants={variants} // Pass the variant object into Framer Motion
+				initial='hidden' // Set the initial state to variants.hidden
+				animate='enter' // Animated state to variants.enter
+				exit='exit' // Exit state (used later) to variants.exit
+				transition={{ type: 'linear', duration: 0.3 }} // Set the transition to linear
+				className='mb-auto'
+			>
+				{children}
+			</motion.main>
 			{showFooter && <Footer />}
 		</div>
 	)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
+import { AnimatePresence } from 'framer-motion'
 
 import type { AppProps /*, AppContext */ } from 'next/app'
 import tailwindConfig from '../tailwind.config'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
 	return (
 		<div className='font-sans text-gray-dark bg-gray-bg text-base break-words min-h-screen'>
 			<Head>
@@ -13,7 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 				{/* background color for mobiles */}
 				<meta name='theme-color' content={tailwindConfig.theme.extend.colors.gray.bg} />
 			</Head>
-			<Component {...pageProps} />
+			<AnimatePresence exitBeforeEnter>
+				<Component {...pageProps} key={router.route} />
+			</AnimatePresence>
 		</div>
 	)
 }
